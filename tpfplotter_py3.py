9d6935ab64dcf55ca5768370a7dd4419f6a4b354
SLSkrzypinski <67860085+SLSkrzypinski@users.noreply.github.com>
2021-10-27 17:35:54 +0200

    Update tpfplotter_py3.py
    Last version of tpfplotter (https://github.com/jlillo/tpfplotter)

--- tpfplotter_py3.py
+++ tpfplotter_py3.py
@@ -1,7 +1,12 @@
+from __future__ import print_function
+import __future__
 import os
 import sys
 import time
-import __future__
+import warnings
+
+import numpy as np
+import argparse
 
 from lightkurve import search_targetpixelfile
 from lightkurve import search_tesscut
@@ -10,12 +15,10 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
 from matplotlib.colorbar import Colorbar
 from matplotlib import patches
-import numpy as np
 import matplotlib.gridspec as gridspec
 from bokeh.io import export_png
 from bokeh.io.export import get_screenshot_as_png
-import warnings
-import numpy as np
+
 from astropy.stats import sigma_clip
 from astropy.coordinates import SkyCoord, Angle
 import astropy.units as u
@@ -25,13 +28,10 @@ from astropy.visualization.mpl_normalize import ImageNormalize
 from astropy.table import Table, Column, MaskedColumn
 from astropy.io import ascii
 from astroquery.mast import Catalogs
-import argparse
 
 def cli():
     """command line inputs
-
     Get parameters from command line
-
     Returns
     -------
     Arguments passed by command line
@@ -41,10 +41,12 @@ def cli():
     parser.add_argument("-L", "--LIST", help="Only fit the LC", action="store_true")
     parser.add_argument("-S", "--SAVEGAIA", help="Save Gaia sources", action="store_true")
     parser.add_argument("-C", "--COORD", help="Use coordinates", default=False)
+    parser.add_argument("-n", "--name", help="Target name to be plotted in title", default=False)
     parser.add_argument("--maglim", default=5., help="Maximum magnitude contrast respect to TIC")
     parser.add_argument("--sector", default=None, help="Select Sector if more than one")
     parser.add_argument("--gid", default=None, help="Gaia ID")
     parser.add_argument("--gmag", default=None, help="Gaia mag")
+    parser.add_argument("--legend", default='best', help="Legend location")
     args = parser.parse_args()
     return args
 
@@ -72,13 +74,13 @@ def add_gaia_figure_elements(tpf, magnitude_limit=18,targ_mag=10.):
     result = result[result.Gmag < magnitude_limit]
     if len(result) == 0:
         raise no_targets_found_message
-    radecs = np.vstack([result['RA_ICRS'], result['DE_ICRS']]).T
-    coords = tpf.wcs.all_world2pix(radecs, 0.5) ## TODO, is origin supposed to be zero or one?
-    year = ((tpf.astropy_time[0].jd - 2457206.375) * u.day).to(u.year)
-    pmra = ((np.nan_to_num(np.asarray(result.pmRA)) * u.milliarcsecond/u.year) * year).to(u.arcsec).value
-    pmdec = ((np.nan_to_num(np.asarray(result.pmDE)) * u.milliarcsecond/u.year) * year).to(u.arcsec).value
+    year = ((tpf.time[0].jd - 2457206.375) * u.day).to(u.year)
+    pmra = ((np.nan_to_num(np.asarray(result.pmRA)) * u.milliarcsecond/u.year) * year).to(u.degree).value
+    pmdec = ((np.nan_to_num(np.asarray(result.pmDE)) * u.milliarcsecond/u.year) * year).to(u.degree).value
     result.RA_ICRS += pmra
     result.DE_ICRS += pmdec
+    radecs = np.vstack([result['RA_ICRS'], result['DE_ICRS']]).T
+    coords = tpf.wcs.all_world2pix(radecs, 0.5) ## TODO, is origin supposed to be zero or one?
 
     # Gently size the points by their Gaia magnitude
     sizes = 128.0 / 2**(result['Gmag']/targ_mag)#64.0 / 2**(result['Gmag']/5.0)
@@ -91,37 +93,43 @@ def add_gaia_figure_elements(tpf, magnitude_limit=18,targ_mag=10.):
 def plot_orientation(tpf):
 	"""
     Plot the orientation arrows
-
     Returns
     -------
     tpf read from lightkurve
-
 	"""
 	mean_tpf = np.mean(tpf.flux,axis=0)
 	nx,ny = np.shape(mean_tpf)
-	x0,y0 = tpf.column+int(0.9*nx),tpf.row+int(0.2*nx)
+	x0,y0 = tpf.column+int(0.2*nx),tpf.row+int(0.2*ny)
 	# East
 	tmp =  tpf.get_coordinates()
 	ra00, dec00 = tmp[0][0][0][0], tmp[1][0][0][0]
 	ra10,dec10 = tmp[0][0][0][-1], tmp[1][0][0][-1]
-	theta = np.arctan((dec10-dec00)/(ra10-ra00))
+    # Each degree of RA is not a full degree on the sky if not
+    # at equator; need cos(dec) factor to compensate
+	cosdec = np.cos(np.deg2rad(0.5*(dec10+dec00)))
+    # Reverse the order of RA arguments here relative to dec
+    # args to account for handedness of RA/Dec vs. x/y coords:
+	theta = np.arctan((dec10-dec00)/(cosdec*(ra00-ra10)))
 	if (ra10-ra00) < 0.0: theta += np.pi
 	#theta = -22.*np.pi/180.
+    # If angle is small, arrows can be a bit closer to corner:
+	if (abs(np.rad2deg(theta)) < 30):
+		x0 -= 0.08*nx
+		y0 -= 0.08*ny
 	x1, y1 = 1.*np.cos(theta), 1.*np.sin(theta)
 	plt.arrow(x0,y0,x1,y1,head_width=0.2,color='white')
-	plt.text(x0+1.5*x1,y0+1.5*y1,'E',color='white')
+	plt.text(x0+1.6*x1,y0+1.6*y1,'E',color='white',ha='center',va='center')
 	# North
 	theta = theta +90.*np.pi/180.
 	x1, y1 = 1.*np.cos(theta), 1.*np.sin(theta)
 	plt.arrow(x0,y0,x1,y1,head_width=0.2,color='white')
-	plt.text(x0+1.5*x1,y0+1.5*y1,'N',color='white')
+	plt.text(x0+1.6*x1,y0+1.6*y1,'N',color='white',ha='center',va='center')
 
 
 
 def get_gaia_data(ra, dec):
     """
     Get Gaia parameters
-
     Returns
     -------
     RA, DEC
@@ -158,7 +166,6 @@ def get_gaia_data(ra, dec):
 def get_gaia_data_from_tic(tic):
     '''
     Get Gaia parameters
-
     Returns
     -----------------------
     GaiaID, Gaia_mag
@@ -181,7 +188,6 @@ def get_gaia_data_from_tic(tic):
 def get_coord(tic):
 	"""
 	Get TIC corrdinates
-
 	Returns
 	-------
 	TIC number
@@ -233,11 +239,14 @@ if __name__ == "__main__":
         	print('Working on TIC'+tic+' (ra = '+str(ra)+', '+'dec = '+str(dec)+') ...')
 
         if args.gid != None:
-        	gaia_id, mag = args.gid, np.float(args.gmag)
+        	gaia_id, mag = args.gid, float(args.gmag)
         else:
-        	#gaia_id, mag = get_gaia_data(ra, dec)
-            gaia_id, mag = get_gaia_data_from_tic(tic)
-
+            if args.COORD  is not False:
+        	       gaia_id, mag = get_gaia_data(ra, dec)
+            else:
+                gaia_id, mag = get_gaia_data_from_tic(tic)
+                if np.isnan(mag):
+                    gaia_id, mag = get_gaia_data(ra, dec)
 
 
         # By coordinates -----------------------------------------------------------------
@@ -284,9 +293,10 @@ if __name__ == "__main__":
         mean_tpf = np.mean(tpf.flux,axis=0)
         nx,ny = np.shape(mean_tpf)
         norm = ImageNormalize(stretch=stretching.LogStretch())
-        division = np.int(np.log10(np.nanmax(tpf.flux)))
-        splot = plt.imshow(np.nanmean(tpf.flux,axis=0)/10**division,norm=norm, \
-        				extent=[tpf.column,tpf.column+ny,tpf.row,tpf.row+nx],origin='bottom', zorder=0)
+        division = int(np.log10(np.nanmax(np.nanmean(tpf.flux * u.s/u.electron,axis=0))))
+        image = np.nanmean(tpf.flux,axis=0)/10**division
+        splot = plt.imshow(image,norm=norm, \
+        				extent=[tpf.column,tpf.column+ny,tpf.row,tpf.row+nx],origin='lower', zorder=0)
 
         # Pipeline aperture
         if pipeline == "True":                                           #
@@ -310,7 +320,7 @@ if __name__ == "__main__":
         										   1, 1, color=maskcolor, fill=False,alpha=1,lw=2))
 
         # Gaia sources
-        r, res = add_gaia_figure_elements(tpf,magnitude_limit=mag+np.float(args.maglim),targ_mag=mag)
+        r, res = add_gaia_figure_elements(tpf,magnitude_limit=mag+float(args.maglim),targ_mag=mag)
         x,y,gaiamags = r
         x, y, gaiamags=np.array(x)+0.5, np.array(y)+0.5, np.array(gaiamags)
         size = 128.0 / 2**((gaiamags-mag))
@@ -322,16 +332,16 @@ if __name__ == "__main__":
 
         # Legend
         add = 0
-        if np.int(args.maglim) % 2 != 0:
+        if int(args.maglim) % 2 != 0:
             add = 1
-        maxmag = np.int(args.maglim) + add
-        legend_mags = np.linspace(-2,maxmag,np.int((maxmag+2)/2+1))
+        maxmag = int(args.maglim) + add
+        legend_mags = np.linspace(-2,maxmag,int((maxmag+2)/2+1))
         fake_sizes = mag + legend_mags #np.array([mag-2,mag,mag+2,mag+5, mag+8])
         for f in fake_sizes:
             size = 128.0 / 2**((f-mag))
-            plt.scatter(0,0,s=size,c='red',alpha=0.6, edgecolor=None,zorder = 10,label = r'$\Delta m=$ '+str(np.int(f-mag)))
+            plt.scatter(0,0,s=size,c='red',alpha=0.6, edgecolor=None,zorder = 10,label = r'$\Delta m=$ '+str(int(f-mag)))
 
-        ax1.legend(fancybox=True, framealpha=0.7)
+        ax1.legend(fancybox=True, framealpha=0.7, loc=args.legend)
 
         # Source labels
         dist = np.sqrt((x-x[this])**2+(y-y[this])**2)
@@ -344,14 +354,17 @@ if __name__ == "__main__":
         plot_orientation(tpf)
 
         # Labels and titles
-        plt.xlim(tpf.column,tpf.column+ny)
+        # Reverse x limits so that image plots as seen on the sky:
+        plt.xlim(tpf.column+ny,tpf.column)
         plt.ylim(tpf.row,tpf.row+nx)
-        plt.xlabel('Pixel Column Number', fontsize=16)
-        plt.ylabel('Pixel Row Number', fontsize=16)
+        plt.xlabel('Pixel Column Number', fontsize=16, zorder=200)
+        plt.ylabel('Pixel Row Number', fontsize=16, zorder=200)
         if args.COORD is not False:                                                                                          #
-        	plt.title('Coordinates '+tic+' - Sector '+str(tpf.sector), fontsize=16)# + ' - Camera '+str(tpf.camera))  #
-        #else:   												#
-        	#plt.title('TIC '+tic+' - Sector '+str(tpf.sector), fontsize=16)# + ' - Camera '+str(tpf.camera))
+        	plt.title('Coordinates '+tic+' - Sector '+str(tpf.sector), fontsize=16, zorder=200)# + ' - Camera '+str(tpf.camera))  #
+        elif args.name is not False:
+            plt.title(args.name +' - Sector '+str(tpf.sector), fontsize=16, zorder=200)
+        else:   												#
+        	plt.title('TIC '+tic+' - Sector '+str(tpf.sector), fontsize=16, zorder=200)# + ' - Camera '+str(tpf.camera))
 
         # Colorbar
         cbax = plt.subplot(gs[0,1]) # Place it where it should be.
@@ -359,12 +372,15 @@ if __name__ == "__main__":
         pos2 = [pos1.x0 - 0.05, pos1.y0 ,  pos1.width, pos1.height]
         cbax.set_position(pos2) # set a new position
 
-        cb = Colorbar(ax = cbax, mappable = splot, orientation = 'vertical', ticklocation = 'right')
+        cbar_ticks = np.linspace(np.min(image), np.max(image), 8, endpoint=True)
+
+        cb = Colorbar(ax = cbax, mappable = splot, orientation = 'vertical',
+                      ticklocation = 'right')
         plt.xticks(fontsize=14)
+        #cbax.set_yticklabels(["{:4.2f}".format(i) for i in cbar_ticks])
         exponent = r'$\times 10^'+str(division)+'$'
         cb.set_label(r'Flux '+exponent+r' (e$^-$)', labelpad=10, fontsize=16)
 
-        #plt.savefig('TPF_Gaia_TIC'+tic+'_S'+str(tpf.sector)+'.pdf')
         plt.savefig('TIC_'+tic+'_S_'+str(tpf.sector)+'_tpf.png')
 
         # Save Gaia sources info
